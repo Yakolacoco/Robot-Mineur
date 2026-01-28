649643452681ad2fc0c5e23ca9e0dd5f13503639
lucas <133487019+Yakolacoco@users.noreply.github.com>
2026-01-28 20:25:56 +0100

    Refactor Robot class to improve position handling

--- Robot.java
+++ Robot.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Robot {
 
     private String nom;
@@ -8,72 +6,57 @@ public class Robot {
     private int mineraiTransporte;
     private int capaciteMax;
     private int energie;
-
-    private static final int xDepart = 2;
-    private static final int yDepart = 3;
     private static final int energieMax = 100;
-    private static final int stockageMax = 3;
-
-    private Scanner scanner = new Scanner(System.in);
 
     public Robot(String nom, int x, int y, int capaciteMax) {
         this.nom = nom;
-        this.x = xDepart;
-        this.y = yDepart;
-        this.capaciteMax = stockageMax;
+        this.x = x;
+        this.y = y;
+        this.capaciteMax = capaciteMax;
         this.mineraiTransporte = 0;
         this.energie = energieMax;
     }
 
-    public void deplacer(String direction) {
-        switch (direction) {
-            case "D": x++; break;
-            case "G": x--; break;
-            case "B": y++; break;
-            case "H": y--; break;
-            default:
-                System.out.println("Commande invalide !");
-                return;
-        }
-        this.energie -= 10;
+    public void deplacer(int nouvelleX, int nouvelleY) {
+        this.x = nouvelleX;
+        this.y = nouvelleY;
+        this.energie -= 5;
         if (energie < 0) energie = 0;
-        System.out.println("Le robot c'est deplacé dans la direction choisie !");
+        System.out.println("Robot déplacé en : " + x + ", " + y);
     }
 
     public boolean collecter() {
         if (this.mineraiTransporte < this.capaciteMax) {
             this.mineraiTransporte++;
-            System.out.println("Vous avez collectez un minerai ! ");
+            this.energie -= 2;
+            System.out.println("Minerai collecté !");
             return true;
-        } else  {
-            System.out.println("Votre inventaire est plein allez le videz ! ");
+        } else {
+            System.out.println("Inventaire plein !");
             return false;
         }
     }
 
     public int deposer() {
         if (this.mineraiTransporte == 0) {
-            System.out.println("Vous n'avez aucun minerai à déposer.");
+            System.out.println("Rien à déposer.");
             return 0;
         }
         int aDepose = this.mineraiTransporte;
-        System.out.println("Vous avez déposé " + aDepose + " minerais dans l'entrepôt.");
         this.mineraiTransporte = 0;
         return aDepose;
     }
 
     public void reposer() {
         energie = energieMax;
-        System.out.println("Le robot s'est reposé. Énergie maximale : " + energie);
+        System.out.println("Robot reposé, énergie au max.");
     }
 
     public void afficherEtat() {
-        System.out.println("=== État du robot ===");
-        System.out.println("Nom : " + nom);
-        System.out.println("Position : (" + x + ", " + y + ")");
-        System.out.println("Minerai transporté : " + mineraiTransporte + "/" + capaciteMax);
-        System.out.println("Énergie : " + energie + "/" + energieMax);
-        System.out.println("====================");
+        System.out.println("Robot " + nom + " (" + x + "," + y + ") | Sac: " + mineraiTransporte + "/" + capaciteMax + " | Energie: " + energie);
     }
-}
 
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getEnergie() { return energie; }
+}
